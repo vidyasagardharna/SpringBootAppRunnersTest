6b190017ed4b025259e3c45c38acb06d2b0f1569
vikram <vikramdarna12@gmail.com>
2019-06-09 09:53:35 +0530

    App runner commit

---
+++ SpringBootProfilesAppRunnersTest/src/main/java/com/app/SpringBootProfilesAppRunnersTestApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfilesAppRunnersTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProfilesAppRunnersTestApplication.class, args);
+	}
+
+}
---
+++ SpringBootProfilesAppRunnersTest/src/main/java/com/app/runner/AppRunner.java
@@ -0,0 +1,37 @@
+package com.app.runner;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements ApplicationRunner{
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		//option args
+		Set<String> names = args.getOptionNames();
+		System.out.println(names);
+		
+		List<String> optionValues = args.getOptionValues("name");
+		
+		System.out.println(optionValues);
+		
+		boolean containsOption = args.containsOption("branch");
+		System.out.println(containsOption);
+		
+		//non option args
+		List<String> nonOptionArgs = args.getNonOptionArgs();
+		System.out.println(nonOptionArgs);
+		
+		//source args
+		String[] sourceArgs = args.getSourceArgs();
+		System.out.println(Arrays.asList(sourceArgs));
+		
+	}
+
+}
